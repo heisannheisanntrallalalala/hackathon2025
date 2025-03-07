889b694181e8e41f4486ad6b0ad833ecbc9d9587
heisannheisanntrallalalala <andornagy3112@gmail.com>
2025-03-07 18:18:58 +0100

    a

--- public/client.js
+++ public/client.js
@@ -10,14 +10,3 @@ window.onload = () => {
         }
     });
 };
-
-
-
-const validIngredients = [
-    "eple", "appelsin", "banan", "jordbær", "blåbær", "sitron", "lime",
-    "tomat", "agurk", "gulrot", "løk", "hvitløk", "potet", "brokkoli", "paprika", "spinat", "salat",
-    "kylling", "kalkun", "biff", "svinekjøtt", "lammekjøtt", "laks", "torsk", "reker", "egg",
-    "melk", "yoghurt", "fløte", "smør", "ost", "cottage cheese",
-    "pasta", "ris", "brød", "mel", "sukker", "salt", "pepper", "olivenolje", "rapsolje",
-    "bønner", "kikerter", "linser", "mais", "erter"
-];
