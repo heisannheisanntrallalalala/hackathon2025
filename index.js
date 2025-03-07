609a6f22b76b7e5adbd8b0a9c40bb5bc762cd2d8
heisannheisanntrallalalala <andornagy3112@gmail.com>
2025-03-07 18:37:31 +0100

    awd

--- index.js
+++ index.js
@@ -14,8 +14,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 /* ****** */
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+    res.redirect("/home");
+});
+
+app.get("/home", (req, res) => {
     res.sendFile(path.join(__dirname, "public/main.html"));
+});
+
+app.get("/404", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/404.html"));
+});
+
+app.use((req, res) => {
+    res.redirect("/404");
 });
 
 /* ****** */
